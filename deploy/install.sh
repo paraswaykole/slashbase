194fed84240300b3fbb623cd643571103352b910
Paras <paraswaykole12@gmail.com>
2021-10-02 15:27:37 +0530

    install script WIP

---
+++ deploy/install.sh
@@ -0,0 +1,176 @@
+#!/bin/bash
+mkdir slashbase || cd slashbase
+export WORK_DIR=$(pwd)
+
+# WIP: this script is work in progress. Avoid running it.
+# wget "github.com/slashbase/slashbase-server/release.tar.gz" && wget "github.com/slashbase/slashbase-web/release.tar.gz"
+# tar -xf release.tar release.tar
+
+touch template.yaml & cat << EOF > template.yaml
+name: production
+
+server:
+    port: ':3000'
+
+database:
+    host: \${db_host}
+    user: \${db_user}
+    database: \${db_name}
+    password: \${db_pass}
+
+root_user:
+    email: \${root_email}
+    password: \${root_pass}
+
+secret:
+    auth_token_secret: \${auth_secret}
+    crypted_data_secret_key: \${crypted_data_secret}
+
+constants:
+    api_host: "https://\${domain}"
+    app_host: "https://\${domain}"
+EOF
+
+echo "Enter the postgres host (default 127.0.0.1):"
+read db_host
+echo "Enter the postgres database name (default slashbase):"
+read db_name
+echo "Enter the postgres db user (default slashbase):"
+read db_user
+echo "Enter the postgres db password:"
+read db_pass
+echo "Enter the domain/host:"
+read domain
+echo "Enter the root user email (your/admin email):"
+read root_email
+echo "Enter the root user password:"
+read root_pass
+
+auth_secret=$(openssl rand -hex 64)
+crypted_data_secret=$(openssl rand -hex 64)
+
+# create backend configs
+rm -f replace.sed
+touch replace.sed
+variables=( "db_host"
+            $db_host
+            "db_user"
+            $db_user
+            "db_name"
+            $db_name
+            "db_pass"
+            $db_pass
+            "auth_secret"
+            $auth_secret
+            "crypted_data_secret"
+            $crypted_data_secret
+            "domain"
+            $domain
+            "root_email"
+            $root_email
+            "root_pass"
+            $root_pass
+          )
+
+flip=0
+for i in "${variables[@]}"; do
+  if [ $flip -eq 0 ]
+  then
+    printf "s/\${$i}/" >> replace.sed
+    flip=1
+  else
+    printf "$i/\n" >> replace.sed
+    flip=0
+  fi
+done
+
+sed -f replace.sed template.yaml > production.yaml
+rm -f replace.sed template.yaml
+
+# create frontend configs
+touch replace.sed
+variables=( "#API_HOST#"
+            "https:\/\/${domain}"
+          )
+
+flip=0
+for i in "${variables[@]}"; do
+  if [ $flip -eq 0 ]
+  then
+    printf "s/$i/" >> replace.sed
+    flip=1
+  else
+    printf "$i/\n" >> replace.sed
+    flip=0
+  fi
+done
+sed -i -f replace.sed web/config.js
+rm -f replace.sed
+
+# create, install and start slashbase service
+touch slashbase.service
+cat << EOF > slashbase.service
+[Unit]
+Description=Slashbase HTTP API
+After=network.target
+
+[Service]
+Type=simple
+WorkingDirectory=${WORK_DIR}
+ExecStart=${WORK_DIR}/backend -e production
+Restart=on-failure
+LimitNOFILE=65536
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo mv slashbase.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl start slashbase.service
+sudo systemctl enable slashbase.service
+rm -f slashbase.service
+
+# install nginx
+sudo apt update
+sudo apt install nginx
+
+# create frontend html folder
+sudo mkdir -p /var/www/$domain/html
+sudo chown -R $USER:$USER /var/www/$domain/html
+sudo chmod -R 755 /var/www/$domain
+sudo mv $WORK_DIR/web/* /var/www/$domain/html/
+
+# setup nginx configs
+touch $domain
+cat << EOF > $domain
+server {
+        listen 80;
+        listen [::]:80;
+
+        root /var/www/${domain}/html;
+        index index.html index.htm index.nginx-debian.html;
+
+        server_name ${domain};
+
+        location / {
+            try_files \$uri /index.html;
+        }
+
+        location /api {
+            proxy_pass http://localhost:3000;
+            proxy_http_version 1.1;
+            proxy_set_header Upgrade \$http_upgrade;
+            proxy_set_header Connection 'upgrade';
+            proxy_set_header Host \$host;
+            proxy_cache_bypass \$http_upgrade;
+        }
+}
+EOF
+sudo mv $domain /etc/nginx/sites-available/
+sudo ln -s /etc/nginx/sites-available/$domain /etc/nginx/sites-enabled/
+sudo systemctl restart nginx
+
+# install certbot for SSL
+sudo snap install --classic certbot
+sudo ln -s /snap/bin/certbot /usr/bin/certbot
+sudo certbot --nginx
