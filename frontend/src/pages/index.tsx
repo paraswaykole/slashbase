e4c4259a573f06471382bc75fe93ef08e9d5bfc4
Paras Waykole <paraswaykole12@gmail.com>
2022-10-21 13:59:30 +0530

    homepage fix grid issue

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -10,17 +10,30 @@ import { selectProjects } from '../redux/projectsSlice'
 const HomePage: NextPage = () => {
 
   const projects: Project[] = useAppSelector(selectProjects)
+  const projectsGrid: Project[][] = []
+
+  projects.forEach((project: Project, index: number) => {
+    let x = Math.floor(index / 3)
+    let y = index % 3
+    if (!projectsGrid[x]) {
+      projectsGrid[x] = []
+    }
+    projectsGrid[x][y] = project
+  })
 
   return (
     <AppLayout title="Home">
       <h1>All Projects</h1>
-      <div className="columns">
-        {projects.map((project: Project) => (
-          <div className="column">
-            <ProjectCard key={project.id} project={project} />
-          </div>
-        ))}
-      </div>
+      {projectsGrid.map((list: Project[]) => (
+        <div className="columns">
+          {list.map((project: Project) => (
+            <div className="column">
+              <ProjectCard key={project.id} project={project} />
+            </div>
+          ))}
+        </div>
+      ))}
+
       <CreateNewProjectCard />
     </AppLayout >
   )
