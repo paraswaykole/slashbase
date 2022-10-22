069d79a781e94e9c9ce5bb505027d89283f35073
Paras Waykole <paraswaykole12@gmail.com>
2022-10-22 18:54:16 +0530

    frontend mongo data model names

--- frontend/src/components/cards/dbdatamodelcard/dbdatamodelcard.tsx
+++ frontend/src/components/cards/dbdatamodelcard/dbdatamodelcard.tsx
@@ -3,23 +3,27 @@ import React from 'react'
 import { DBConnection, DBDataModel } from '../../../data/models'
 import Constants from '../../../constants'
 import Link from 'next/link'
+import { DBConnType } from '../../../data/defaults'
 
-type DBDataModelPropType = { 
+type DBDataModelPropType = {
     dbConnection: DBConnection
     dataModel: DBDataModel
 }
 
-const DBDataModelCard = ({dataModel, dbConnection}: DBDataModelPropType) => {
+const DBDataModelCard = ({ dataModel, dbConnection }: DBDataModelPropType) => {
 
     return (
-        <div className={"card "+styles.cardContainer}>
-            <Link 
-                href={{pathname: Constants.APP_PATHS.DB_PATH.path, query: {mschema: dataModel.schemaName, mname: dataModel.name}}} 
-                as={Constants.APP_PATHS.DB_PATH.path.replace('[id]', dbConnection.id).replace('[path]', String('data'))+"?mschema="+dataModel.schemaName+"&mname="+dataModel.name}
-                >
+        <div className={"card " + styles.cardContainer}>
+            <Link
+                href={{ pathname: Constants.APP_PATHS.DB_PATH.path, query: { mschema: dataModel.schemaName, mname: dataModel.name } }}
+                as={Constants.APP_PATHS.DB_PATH.path.replace('[id]', dbConnection.id).replace('[path]', String('data')) + "?mschema=" + dataModel.schemaName + "&mname=" + dataModel.name}
+            >
                 <a className={styles.cardLink}>
                     <div className="card-content">
-                        <b>{dataModel.schemaName}.{dataModel.name}</b>
+                        {dbConnection.type === DBConnType.POSTGRES &&
+                            <b>{dataModel.schemaName}.{dataModel.name}</b>}
+                        {dbConnection.type === DBConnType.MONGO &&
+                            <b>{dataModel.name}</b>}
                     </div>
                 </a>
             </Link>
