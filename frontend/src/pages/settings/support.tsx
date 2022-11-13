92a8e90fd4f606786f1e4e45205fcc4c30e4bcfc
Paras Waykole <paraswaykole12@gmail.com>
2022-11-13 12:52:40 +0530

    fix support links

--- frontend/src/pages/settings/support.tsx
+++ frontend/src/pages/settings/support.tsx
@@ -17,7 +17,7 @@ const SupportPage: NextPage = () => {
             <p>If you are have any questions or ideas,
                 please join our Discord server and share. We are open to discussing anything your questions or ideas.</p>
             <Link href={Constants.EXTERNAL_PATHS.DISCORD_COMMUNITY}>
-                <a>
+                <a target="_blank">
                     <button className="button is-secondary">
                         <i className={"fab fa-discord"} />&nbsp;&nbsp;Join Discord Community
                     </button>
@@ -28,7 +28,7 @@ const SupportPage: NextPage = () => {
             <p>If you are getting any errors or bugs, join our Discord server and ask for help.
                 We will try to fix the errors bugs in the next version release.</p>
             <Link href={Constants.EXTERNAL_PATHS.REPORT_BUGS}>
-                <a>
+                <a target="_blank">
                     <button className="button is-secondary">
                         <i className={"fab fa-github"} />&nbsp;&nbsp;File a GitHub Issue.
                     </button>
@@ -38,7 +38,7 @@ const SupportPage: NextPage = () => {
             <h4>Want to check what&apos;s new?</h4>
             <p>Vist our website to check new releases.</p>
             <Link href={Constants.EXTERNAL_PATHS.OFFICIAL_WEBSITE}>
-                <a>
+                <a target="_blank">
                     <button className="button is-secondary">
                         <i className={"fas fa-globe"} />&nbsp;&nbsp;Visit Slashbase.com
                     </button>
