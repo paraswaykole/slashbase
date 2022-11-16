e27bd7f1ec66dbfd983461ec9d64ecb41391e2c4
Paras Waykole <paraswaykole12@gmail.com>
2022-11-16 13:03:07 +0530

    fix minor issue

--- frontend/src/network/request.ts
+++ frontend/src/network/request.ts
@@ -6,19 +6,23 @@ import reduxStore from '../redux/store'
 const getApiInstance = () => {
     const apiInstance: AxiosInstance = axios.create({
         baseURL: GetAPIConfig().API_URL,
-        headers: {'content-type': 'text/json'},
+        headers: { 'content-type': 'text/json' },
         withCredentials: true,
     })
 
     apiInstance.interceptors.response.use(
         async function (response: AxiosResponse<any>) {
             return Promise.resolve(response)
-        }, 
+        },
         async function (error: any) {
             const status = error.status || error.response.status;
-            if (status === 401){
+            if (status === 401) {
                 const { dispatch } = reduxStore
                 await dispatch(clearLogin())
+                return Promise.resolve(error.response)
+            }
+            if (status === 500) {
+                // TODO: move error toasts here
                 return Promise.resolve(error.response)
             }
             return Promise.reject(error)
