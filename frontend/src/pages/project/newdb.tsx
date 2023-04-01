7f35301abb258b2358bf0dfb4029bb7bfc51be28
Tarun Samanta <tarunsamanta77@gmail.com>
2023-04-01 11:28:05 +0530

    modified PR as per your request

--- frontend/src/pages/project/newdb.tsx
+++ frontend/src/pages/project/newdb.tsx
@@ -21,9 +21,7 @@ const NewDBPage: FunctionComponent<{}> = () => {
     const [addingError, setAddingError] = useState(false)
     const [adding, setAdding] = useState(false)
     const [inputError, setInputError] = useState({
-        error_1: false, error_2: false, error_3: false, error_4: false, error_5: false, 
-        error_6: false, error_7: false, error_8: false, error_9: false, error_10: false,
-        error_11: false
+        error_1: false, error_2: false, error_3: false, error_4: false
     })
     const [data, setData] = useState({
         dbName: "",
@@ -48,43 +46,17 @@ const NewDBPage: FunctionComponent<{}> = () => {
 
         switch (name) {
             case "dbName":
-              (e.target.value.trim().length>=1) ? setInputError({...inputError, error_1: false}): setInputError({...inputError, error_1: true})
+              (e.target.value.trim().length>0) ? setInputError({...inputError, error_1: false}): setInputError({...inputError, error_1: true})
               break;
-            case "dbScheme":
-               (e.target.value.localeCompare("mongodb")==0 || e.target.value.localeCompare("mongodb+srv")==0) ? 
-               setInputError({...inputError, error_2: false}): setInputError({...inputError, error_2: true})
-                break;
             case "dbHost":
-                (e.target.value.trim().length>0) ? setInputError({...inputError, error_3: false}): setInputError({...inputError, error_3: true})
+                (e.target.value.trim().length>0) ? setInputError({...inputError, error_2: false}): setInputError({...inputError, error_2: true})
                 break;
             case "dbPort":
-                (e.target.value.trim().length>0) ? setInputError({...inputError, error_4: false}): setInputError({...inputError, error_4: true})
+                (e.target.value.trim().length>0) ? setInputError({...inputError, error_3: false}): setInputError({...inputError, error_3: true})
                 break;
             case "dbDatabase":
-                (e.target.value.trim().length>0) ? setInputError({...inputError, error_5: false}): setInputError({...inputError, error_5: true})
+                (e.target.value.trim().length>0) ? setInputError({...inputError, error_4: false}): setInputError({...inputError, error_4: true})
                 break;
-            case "dbUsername":
-                (e.target.value.trim().length>0) ? setInputError({...inputError, error_6: false}): setInputError({...inputError, error_6: true})
-                break;
-            case "dbPassword":
-                (e.target.value.trim().length>0) ? setInputError({...inputError, error_7: false}): setInputError({...inputError, error_7: true})
-                break;
-            case "dbSSHHost":
-                (e.target.value.trim().length>0) ? 
-                setInputError({...inputError, error_8:false}) : setInputError({...inputError, error_8: true})
-                break;
-            case "dbSSHUser":
-                (e.target.value.trim().length>0) ? 
-                setInputError({...inputError, error_9:false}) : setInputError({...inputError, error_9: true})
-                break;
-            case "dbSSHPassword":
-                (e.target.value.trim().length>0) ? 
-                setInputError({...inputError, error_10:false}) : setInputError({...inputError, error_10: true})
-                break;
-            case "dbSSHKeyFile":
-                (e.target.value.trim().length>0) ? 
-                setInputError({...inputError, error_11:false}) : setInputError({...inputError, error_11: true})
-                break;   
           }
 
         const value = type === "checkbox"
@@ -128,23 +100,13 @@ const NewDBPage: FunctionComponent<{}> = () => {
             await dispatch(addNewDBConn(payload)).unwrap()
             navigate(Constants.APP_PATHS.PROJECT.path.replace('[id]', project.id))
         } catch (e: any) {
-            var f1=false, f2=false, f3=false, f4=false, f5=false, f6=false, f7=false, 
-            f8=false, f9=false, f10=false, f11=false;
+            var f1=false, f2=false, f3=false, f4=false;
             (payload.name.length===0) ? f1=true : f1=false;
-            (payload.scheme.localeCompare("mongodb") == 0 ||  payload.scheme.localeCompare("mongodb+srv") == 0) ?
-             f2=false : f2= true;
-            (payload.host.length===0) ? f3=true : f3=false;
-            (payload.port.length===0) ? f4=true : f4=false;
-            (payload.dbname.length===0) ? f5=true : f5=false;
-            (payload.user.length===0) ? f6=true : f6=false;
-            (payload.password.length===0) ? f7=true : f7=false;
-            (payload.sshHost.length===0) ? f8=true : f8=false;
-            (payload.sshUser.length===0) ? f9=true : f9=false;
-            (payload.sshPassword.length===0) ? f10=true : f10=false;
-            (payload.sshKeyFile.length===0) ? f11=true : f11=false;
+            (payload.host.length===0) ? f2=true : f2=false;
+            (payload.port.length===0) ? f3=true : f3=false;
+            (payload.dbname.length===0) ? f4=true : f4=false;
             setInputError({...inputError,error_1: f1,error_2: f2,error_3: f3,
-             error_4: f4, error_5: f5, error_6: f6, error_7: f7, error_8: f8,
-            error_9: f9, error_10: f10, error_11: f11})
+             error_4: f4})
             setAddingError(e)
         }
         
@@ -175,8 +137,8 @@ const NewDBPage: FunctionComponent<{}> = () => {
                     <label className="label">Database Type:</label>
                     <div className="control">
                         <div className="select">
-                            <select name="dbType" onChange={(e: React.ChangeEvent<HTMLSelectElement>) => { setData((prev) => ({ ...prev, [e.target.name]: e.target.value, dbScheme: "" })) }}>
-                                <option value={DBConnType.POSTGRES}>PostgreSQL</option>
+                            <select name="dbType" style= {normal} onChange={(e: React.ChangeEvent<HTMLSelectElement>) => { setData((prev)=> ({...prev, [e.target.name]:e.target.value, dbScheme :""}))}}>
+                                <option value={DBConnType.POSTGRES}>PostgresSQL</option>
                                 <option value={DBConnType.MONGO}>MongoDB</option>
                                 <option value={DBConnType.MYSQL}>MySQL</option>
                             </select>
@@ -187,7 +149,7 @@ const NewDBPage: FunctionComponent<{}> = () => {
                     <label className="label">Scheme:</label>
                     <div className="control">
                         <div className="select">
-                            <select name='dbScheme' style={inputError.error_2 ? inputStyle :  normal} onChange={e => handleChange(e)}>
+                            <select name='dbScheme' style= {normal} onChange={e => handleChange(e)}>
                                 <option value="default">Select scheme</option>
                                 <option value="mongodb">mongodb</option>
                                 <option value="mongodb+srv">mongodb+srv</option>
@@ -201,7 +163,7 @@ const NewDBPage: FunctionComponent<{}> = () => {
                     value={data.dbHost}
                     onChange={e => handleChange(e)}
                     placeholder="Enter host"
-                    style={inputError.error_3 ? inputStyle :  normal}
+                    style={inputError.error_2 ? inputStyle :  normal}
                 />
                 <InputTextField
                     label='Port:'
@@ -209,7 +171,7 @@ const NewDBPage: FunctionComponent<{}> = () => {
                     value={data.dbPort}
                     onChange={e => handleChange(e)}
                     placeholder="Enter Port"
-                    style={inputError.error_4 ? inputStyle :  normal}
+                    style={inputError.error_3 ? inputStyle :  normal}
                 />
                 <InputTextField
                     label='Database Name:'
@@ -217,7 +179,7 @@ const NewDBPage: FunctionComponent<{}> = () => {
                     value={data.dbDatabase}
                     onChange={e => handleChange(e)}
                     placeholder="Enter Database"
-                    style={inputError.error_5 ? inputStyle :  normal}
+                    style={inputError.error_4 ? inputStyle :  normal}
                 />
                 <InputTextField
                     label='Database User:'
@@ -225,7 +187,7 @@ const NewDBPage: FunctionComponent<{}> = () => {
                     value={data.dbUsername}
                     onChange={e => handleChange(e)}
                     placeholder="Enter Database username"
-                    style={inputError.error_6 ? inputStyle :  normal}
+                    style= {normal}
                 />
                 <PasswordInputField
                     label='Database Password:'
@@ -233,7 +195,7 @@ const NewDBPage: FunctionComponent<{}> = () => {
                     value={data.dbPassword}
                     onChange={e => handleChange(e)}
                     placeholder="Enter database password"
-                    style={inputError.error_7 ? inputStyle :  normal}
+                    style= {normal}
                 />
                 <div className="field">
                     <label className="label">Use SSH:</label>
@@ -242,6 +204,7 @@ const NewDBPage: FunctionComponent<{}> = () => {
                             name='dbUseSSH'
                             value={data.dbUseSSH}
                             onChange={e => handleChange(e)}
+                            style= {normal}
                         >
                             <option
                                 value={DBConnectionUseSSHType.NONE} >
@@ -283,7 +246,7 @@ const NewDBPage: FunctionComponent<{}> = () => {
                             value={data.dbSSHHost}
                             onChange={e => handleChange(e)}
                             placeholder="Enter SSH Host"
-                            style={inputError.error_8 ? inputStyle :  normal}
+                            style= {normal}
                             
                         />
                         <InputTextField
@@ -292,7 +255,7 @@ const NewDBPage: FunctionComponent<{}> = () => {
                             value={data.dbSSHUser}
                             onChange={e => handleChange(e)}
                             placeholder="Enter SSH User"
-                            style={inputError.error_9 ? inputStyle :  normal}
+                            style= {normal}
                             
                         />
                         {(data.dbUseSSH === DBConnectionUseSSHType.PASSWORD || data.dbUseSSH === DBConnectionUseSSHType.PASSKEYFILE) &&
@@ -302,7 +265,7 @@ const NewDBPage: FunctionComponent<{}> = () => {
                                 value={data.dbSSHPassword}
                                 onChange={e => handleChange(e)}
                                 placeholder="Enter SSH Password"
-                                style={inputError.error_10 ? inputStyle :  normal}
+                                style= {normal}
                                 
                             />
                         }
@@ -316,7 +279,7 @@ const NewDBPage: FunctionComponent<{}> = () => {
                                         value={data.dbSSHKeyFile}
                                         onChange={e => handleChange(e)}
                                         placeholder="Paste the contents of SSH Identity File here"
-                                        style={inputError.error_11 ? inputStyle :  normal}
+                                        style= {normal}
                                          />
                                 </div>
                             </div>
